5561c95c7685bdf17b6c2606049c188c47680c93
AustinHWalsh <austinhwalsh@gmail.com>
2021-05-26 00:28:18 +1000

    created the database interaction to determine if a user is already logged in

--- goldstein_app/lib/pages/main.dart
+++ goldstein_app/lib/pages/main.dart
@@ -1,16 +1,13 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:goldstein_app/assets/constants.dart';
-import 'package:goldstein_app/assets/error.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:goldstein_app/pages/add_event.dart';
 import 'package:goldstein_app/pages/root.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
-import 'home.dart';
 
 Future<void> main() async {
   // setup sentry error reporting
@@ -35,9 +32,6 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    FirebaseAuth.instance.signInAnonymously().catchError((e) {
-      errorReporter.captureException(e);
-    });
     return MaterialApp(
       title: 'Goldstein App',
       theme: ThemeData(
--- goldstein_app/lib/pages/root.dart
+++ goldstein_app/lib/pages/root.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+import 'package:device_info/device_info.dart';
+import 'package:goldstein_app/userIDs/userIDs_firestore_service.dart';
+import 'package:goldstein_app/userIDs/userIDs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:goldstein_app/assets/error.dart';
 import 'package:goldstein_app/assets/password.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
-
 import 'home.dart';
+import 'main.dart';
 
 class MyRoot extends StatefulWidget {
   @override
@@ -13,9 +19,27 @@ class _MyRootState extends State<MyRoot> {
   final _formKey = GlobalKey<FormState>();
   final _password = TextEditingController();
   final _hasher = PasswordHash();
+  String _deviceId = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _asyncMethod();
+  }
+
+  _asyncMethod() async {
+    await _getUniqId();
+    await _userLogged();
+  }
 
   @override
   Widget build(BuildContext context) {
+    return oneTimePage();
+  }
+
+  // A page that displays the password field and an enter button
+  // push the user's info to the firebase and save it
+  Widget oneTimePage() {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
@@ -56,6 +80,7 @@ class _MyRootState extends State<MyRoot> {
               onPressed: () {
                 if (_formKey.currentState.validate()) {
                   _formKey.currentState.save();
+                  idDBS.create({}, id: _deviceId);
                   Navigator.pushReplacement(
                       context,
                       MaterialPageRoute(
@@ -72,5 +97,38 @@ class _MyRootState extends State<MyRoot> {
         ),
       ),
     );
+  }
+
+  // Get the uniqId on each start of the app and check if the user is
+  // already logged in
+  Future<void> _getUniqId() async {
+    var deviceInfo = DeviceInfoPlugin();
+    var uniqID;
+    try {
+      if (Platform.isIOS) {
+        var iosDeviceInfo = await deviceInfo.iosInfo;
+        uniqID = iosDeviceInfo.identifierForVendor; // unique ID on iOS
+      } else {
+        var androidDeviceInfo = await deviceInfo.androidInfo;
+        uniqID = androidDeviceInfo.androidId; // unique ID on Android
+      }
+    } on PlatformException {
+      errorReporter.captureMessage("Failed to get device data");
+      return;
+    }
+    setState(() {
+      _deviceId = uniqID;
+    });
+  }
+
+  // Determine if the user's ID exists in the firebase
+  // If it does, load the home page, otherwise load the login page
+  Future<void> _userLogged() async {
+    if (idDBS.collection.contains(_deviceId)) {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => HomePage(title: 'Goldstein College')));
+    }
   }
 }
